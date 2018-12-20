b77b7f268d450eb2cecb939efa4379a540f11c54
Tom Wilhoit <tomwilhoit.business@gmail.com>
2018-12-20 14:51:03 -0700

    Added more info

--- src/Card.js
+++ src/Card.js
@@ -9,17 +9,51 @@ class Card extends Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      moreInfo : false,
+    }
+
+  }
+
+  getMoreInfo = () =>{
+    this.setState({
+      moreInfo: !this.state.moreInfo
+    })
+
   }
 
   render() {
-    return (
-      <div class='ice-cream-card'>
-          <img class="flavor-img" src={`./images/Flavors/${this.props.img}.jpg`}/>
-          <h1 class="ice-cream-name">{this.props.flavor}</h1>
-          <button class="ice-cream-info-btn">More Info</button>
-      </div>
+   
+   if (this.state.moreInfo === false) {
+     
+     return (
+       <div class='ice-cream-card'>
+           <img class="flavor-img" src={`./images/Flavors/${this.props.img}.jpg`}/>
+           <h1 class="ice-cream-name">{this.props.flavor}</h1>
+           <button onClick={this.getMoreInfo}class="ice-cream-info-btn">More Info</button>
+       </div>
+  
+     )
+   } else {
+   return (
+    
+    <div class='ice-cream-card'>
+    <img class="flavor-img" src={`./images/Flavors/${this.props.img}.jpg`}/>
+    <h1 class="ice-cream-name">{this.props.flavor}</h1>
+    <p>;lansd;foansf;kanrs;fnasr</p>
 
-    )
+    <p>;lansd;foansf;kanrs;fnasr</p>
+
+    <p>;lansd;foansf;kanrs;fnasr</p>
+
+    <p>;lansd;foansf;kanrs;fnasr</p>
+    <button onClick={this.getMoreInfo}class="ice-cream-info-btn">More Info</button>
+</div>
+
+   )
+
+   }
+   
   }
 
 
