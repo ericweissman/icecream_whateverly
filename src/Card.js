f77ec9944f578f6160ab45327f45ba8cd352dabf
Eric Weissman <eric.weissman21@gmail.com>
2018-12-21 15:05:39 -0700

    Found and fixed bug preventing more info functionality

--- src/Card.js
+++ src/Card.js
@@ -2,26 +2,44 @@ import React, { Component } from 'react';
 import App from './App.js';
 import './css/Card.css'
 import { uid } from 'react-uid'
-
+import ParlorArea from './ParlorArea.js';
 // import images from './images/';
 
 class Card extends Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      moreInfo: false,
+    }
+  }
+
+  getMoreInfo = () => {
+    this.setState({
+      moreInfo: !this.state.moreInfo
+    })
   }
 
   render() {
-    return (
-      <div class='ice-cream-card'>
-          <img class="flavor-img" src={`./images/Flavors/${this.props.img}.jpg`}/>
-          <h1 class="ice-cream-name">{this.props.flavor}</h1>
-        <button class=".more-info-btn">More Info</button>
-      </div>
-
-    )
+    if (this.state.moreInfo === false) {
+      return (
+        <div className='ice-cream-card' id={this.props.id}>
+          <img className="flavor-img" src={`./images/Flavors/${this.props.img}.jpg`} />
+          <h1 className="ice-cream-name">{this.props.flavor}</h1>
+          <button onClick={this.getMoreInfo} className="more-info-btn">More Info</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className='ice-cream-card'>
+          <img className="flavor-img" src={`./images/Flavors/${this.props.img}.jpg`} />
+          <h1 className="ice-cream-name">{this.props.flavor}</h1>
+          <button onClick={this.getMoreInfo} className="more-info-btn">Less Info</button>
+          <ParlorArea parlors={this.props.parlors} id={this.props.id} />
+        </div>
+      );
+    }
   }
-
-
 }
 export default Card;
+
