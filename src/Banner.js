bfbcc7fe6479c496b8aadbc6274b07c4d30de7e8
Eric Weissman <eric.weissman21@gmail.com>
2019-01-01 16:51:27 -0700

    Remove styling for app scss

--- src/Banner.js
+++ src/Banner.js
@@ -5,7 +5,8 @@ const Banner = () => {
     return (
       <div className='Banner'>
         <div className='banner-center'>
-          <img src='./images/icecreams.jpg' className='cones-img'/>
+          <img src="http://purepng.com/public/uploads/large/purepng.com-ice-cream-conefood-ice-cream-ice-cream-cone-cone-scoop-vanilla-ice-941524636426f7rep.png"
+         className='cones-img'/>
           <p className='banner-text'>Our mission is to show you the craziest, most interesting, new ice cream flavors on the block around Denver and Aurora. You can use the search bar to locate your favorite ice cream or just browse our homepage to see the coolest new flavors!</p>
         </div>
       </div>
