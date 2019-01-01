05a59ed8346392668aad9cd806aa7b2d2716884e
Heather Hartley <hlhartley@gmail.com>
2019-01-01 11:51:58 -0700

    Merges all conflicts

--- src/Header.js
+++ src/Header.js
@@ -7,23 +7,6 @@ import SearchByParlor from './SearchByParlor';
 class Header extends Component {
   constructor() {
     super();
-<<<<<<< HEAD
-  }
-  render() {
-    return (
-      <header className="header">
-        <div className="header-svg-cont">
-          <img className="header-svg" src={require('./css/images/ice-cream.svg')}/>
-        </div>
-        <div className="header-search-cont">
-        <header className="title">iScream</header>
-          <Search searchFor={this.props.searchFor}/>
-          <SearchByParlor parlors={this.props.parlors} searchParlor={this.props.searchParlor}/>
-        </div>
-      </header>
-    );
-  }
-=======
   }
   render() {
     return (
@@ -37,7 +20,6 @@ class Header extends Component {
       </header>
     );
   }
->>>>>>> 2a92d4c6f21b849ce00ce5bacc1c8b106ee21560
 }
 
 
